3b176df61e58263b78c1a1f48951794a922ea18f
Mosula7 <mosulishviligio7@gmail.com>
2024-07-13 21:51:37 +0400

    fixed formating added train pipline stuff

--- process_data.py
+++ process_data.py
@@ -8,7 +8,9 @@ from feature_engine.encoding import OneHotEncoder
 
 
 class ProcessData:
-    def __init__(self, model_type: str, data_name:str, out_dir:str, test_size:float, target:str, cat_cols:list):
+    def __init__(self, model_type: str, data_name: str, out_dir: str,
+                 test_size: float, target: str, cat_cols: list):
+
         self.model_type = model_type
         self.df = pd.read_csv(data_name)
         self.test_size = test_size
@@ -16,22 +18,31 @@ class ProcessData:
         self.cat_cols = cat_cols
         self.out_dir = out_dir
 
-
     def clean_data(self):
         self.df = self.df.drop(columns=['customerID'])
         self.df['MonthlyCharges'] = self.df['MonthlyCharges'].astype('float64')
-        self.df['TotalCharges'] = self.df['TotalCharges'].replace(' ', '0').astype('float64')
-        self.df['Churn'] = self.df['Churn'].map({'Yes':1, 'No': 0})
+        self.df['TotalCharges'] = self.df['TotalCharges'].replace(' ', '0')
+        self.df['TotalCharges'] = self.df['TotalCharges'].astype('float64')
+        self.df['Churn'] = self.df['Churn'].map({'Yes': 1, 'No': 0})
 
-    
-    def split_data(self, df: pd.DataFrame, target: str, test_size: float, 
-               val_size: float=None, random_state:int = 0):
-        
+    def split_data(self, df: pd.DataFrame, target: str, test_size: float,
+                   val_size: float = None, random_state: int = 0):
+
         if not val_size:
             val_size = test_size / (1 - test_size)
 
-        train_valid, test = train_test_split(df, test_size=test_size, stratify=df[target], random_state=random_state)
-        train, valid = train_test_split(train_valid, test_size=val_size, stratify=train_valid[target], random_state=random_state)
+        train_valid, test = train_test_split(
+            df,
+            test_size=test_size,
+            stratify=df[target],
+            random_state=random_state
+        )
+        train, valid = train_test_split(
+            train_valid,
+            test_size=val_size,
+            stratify=train_valid[target],
+            random_state=random_state
+        )
 
         self.X_train = train[train.columns.drop(target)]
         self.X_valid = valid[valid.columns.drop(target)]
@@ -40,7 +51,6 @@ class ProcessData:
         self.y_train = train[target]
         self.y_valid = valid[target]
         self.y_test = test[target]
-        
 
     def lgb_data_processor(self, cat_cols):
         encoder = OneHotEncoder(
@@ -52,20 +62,30 @@ class ProcessData:
         self.X_valid = encoder.transform(self.X_valid)
         self.X_test = encoder.transform(self.X_test)
 
-
     def cat_data_processor(self):
         pass
 
-    
     def save_data(self, out_dir):
-        for d in ('X_train', 'X_valid', 'X_test', 'y_train', 'y_valid', 'y_test'):
-            self.__getattribute__(d).to_csv(os.path.join(out_dir, f'{d}.csv'), index=False)
-
+        datasets = (
+            'X_train', 'X_valid', 'X_test',
+            'y_train', 'y_valid', 'y_test'
+        )
+        for d in datasets:
+            self.__getattribute__(d).to_csv(
+                os.path.join(out_dir, f'{d}.csv'),
+                index=False
+            )
 
     def run_pipeline(self):
         self.clean_data()
-        self.split_data(df=self.df, test_size=self.test_size, target=self.target)
-        self.__getattribute__(f'{self.model_type}_data_processor')(cat_cols=self.cat_cols)
+        self.split_data(
+            df=self.df,
+            test_size=self.test_size,
+            target=self.target
+        )
+        self.__getattribute__(f'{self.model_type}_data_processor')(
+            cat_cols=self.cat_cols
+        )
         self.save_data(out_dir=self.out_dir)
 
 
@@ -75,12 +95,12 @@ if __name__ == '__main__':
         config = json.load(file)['data']
 
     data_pipeline = ProcessData(
-            model_type=model_type, 
-            data_name=config["data_name"], 
+            model_type=model_type,
+            data_name=config["data_name"],
             out_dir=config["out_dir"],
             test_size=config["test_size"],
             target=config["target"],
             cat_cols=config["cat_cols"]
         )
-    
+
     data_pipeline.run_pipeline()
--- train_model.py
+++ train_model.py
@@ -1,15 +1,59 @@
+import os
+import sys
+import json
+
+import pandas as pd
 import catboost as cat
 import lightgbm as lgb
 
+
 class TrainModel:
-    def __init__(self, model_type):
+    def __init__(self, model_type, data_dir, hyperparams):
         self.model_type = model_type
+        self.data_dir = data_dir
+        self.hyperparams = self.hyperparams
+        datasetsets = (
+            'X_train', 'X_valid', 'X_test',
+            'y_train', 'y_valid', 'y_test'
+        )
+        for d in datasetsets:
+            self.__setattr__(
+                d,
+                pd.read_csv(os.path.join(self.data_dir, f'{d}.csv'))
+            )
+
+    def train_lgb(self):
+        model = lgb.LGBMClassifier(**self.hyperparams)
+        model.fit(
+            self.X_train, self.y_train,
+            eval_set=(self.X_valid, self.y_valid)
+        )
+
+    def train_cat(self):
+        model = cat.CatBoostClassifier(**self.hyperparams)
+        model.fit()
+
+    def evaluate_model(self):
+        pass
+
+    def run_pipeline(self):
         self.__getattribute__(f'train_{self.model_type}')
-    
-
-    def train_lgb():
-        pass
+        self.evaluate_model()
 
 
-    def train_cat():
-        pass
+if __name__ == '__main__':
+    model_type = sys.argv[1]
+
+    with open(os.path.join('configs', f'config_{model_type}.json')) as file:
+        data_dir = json.load(file)['data']['out_dir']
+
+    with open(os.path.join('configs', f'config_{model_type}.json')) as file:
+        hyperparams = json.load(file)['model']
+
+    model_pipeline = TrainModel(
+        model_type=model_type,
+        data_dir=data_dir,
+        hyperparams=hyperparams
+    )
+
+    model_pipeline.run_pipeline()
