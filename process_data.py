d6c347c254e6f93550dbcbc705b81228d46fbc3c
Mosula7 <mosulishviligio7@gmail.com>
2024-07-13 21:19:00 +0400

    finished a pipeline

--- process_data.py
+++ process_data.py
@@ -1,4 +1,7 @@
 import os
+import json
+import sys
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from feature_engine.encoding import OneHotEncoder
@@ -15,6 +18,7 @@ class ProcessData:
 
 
     def clean_data(self):
+        self.df = self.df.drop(columns=['customerID'])
         self.df['MonthlyCharges'] = self.df['MonthlyCharges'].astype('float64')
         self.df['TotalCharges'] = self.df['TotalCharges'].replace(' ', '0').astype('float64')
         self.df['Churn'] = self.df['Churn'].map({'Yes':1, 'No': 0})
@@ -26,22 +30,22 @@ class ProcessData:
         if not val_size:
             val_size = test_size / (1 - test_size)
 
-        train_val, test = train_test_split(df, test_size=test_size, stratify=df[target], random_state=random_state)
-        train, val = train_test_split(train_val, test_size=val_size, stratify=train_val[target], random_state=random_state)
+        train_valid, test = train_test_split(df, test_size=test_size, stratify=df[target], random_state=random_state)
+        train, valid = train_test_split(train_valid, test_size=val_size, stratify=train_valid[target], random_state=random_state)
 
         self.X_train = train[train.columns.drop(target)]
-        self.X_val = val[val.columns.drop(target)]
+        self.X_valid = valid[valid.columns.drop(target)]
         self.X_test = test[test.columns.drop(target)]
 
         self.y_train = train[target]
-        self.y_val = val[target]
+        self.y_valid = valid[target]
         self.y_test = test[target]
         
 
     def lgb_data_processor(self, cat_cols):
         encoder = OneHotEncoder(
-        variables=cat_cols,
-        drop_last_binary=True
+            variables=cat_cols,
+            drop_last_binary=True
         )
 
         self.X_train = encoder.fit_transform(self.X_train)
@@ -55,11 +59,28 @@ class ProcessData:
     
     def save_data(self, out_dir):
         for d in ('X_train', 'X_valid', 'X_test', 'y_train', 'y_valid', 'y_test'):
-            self.__getattribute__(d).to_csv(os.path.join(out_dir, f'{d}.csv'), ignore_index=False)
+            self.__getattribute__(d).to_csv(os.path.join(out_dir, f'{d}.csv'), index=False)
 
 
     def run_pipeline(self):
         self.clean_data()
         self.split_data(df=self.df, test_size=self.test_size, target=self.target)
-        self.__getattribute__(f'{self.model_type}_data_processor', cat_cols=self.cat_cols)
+        self.__getattribute__(f'{self.model_type}_data_processor')(cat_cols=self.cat_cols)
         self.save_data(out_dir=self.out_dir)
+
+
+if __name__ == '__main__':
+    model_type = sys.argv[1]
+    with open(os.path.join('configs', f'config_{model_type}.json')) as file:
+        config = json.load(file)['data']
+
+    data_pipeline = ProcessData(
+            model_type=model_type, 
+            data_name=config["data_name"], 
+            out_dir=config["out_dir"],
+            test_size=config["test_size"],
+            target=config["target"],
+            cat_cols=config["cat_cols"]
+        )
+    
+    data_pipeline.run_pipeline()
